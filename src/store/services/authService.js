35609d9178deab801cd590b2f12ac68d5a86d7db
Tushar <tushar.toshi12@gmail.com>
2022-11-16 17:04:26 +0530

    Changed API route

--- src/store/services/authService.js
+++ src/store/services/authService.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authService = createApi({
   reducerPath: "auth",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://inquiry-management-system-backend.onrender.com/api/',
+    baseUrl: process.env.REACT_APP_API_ROUTE,
   }),
   endpoints: (builder) => {
     return {
